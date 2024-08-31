c5ed235a6f51316c3185076f394542372b8b4009
team-K <reepoo0528@gmail.com>
2024-08-31 22:03:30 +0900

    change file

--- main.go
+++ main.go
@@ -3,5 +3,10 @@ package main
 import "fmt"
 
 func main() {
-    fmt.Println("Hello, World!")
+	fmt.Println("Hello, World!")
+	fmt.Println(10 + 20)
+	fmt.Println(20 - 10)
+	fmt.Println(20 * 10)
+	fmt.Println(20 / 10)
+	fmt.Println(20 % 10)
 }
